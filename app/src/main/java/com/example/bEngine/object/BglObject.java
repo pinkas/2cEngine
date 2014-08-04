0595e9e6628e3aeedd80819552736c0b91a87a6b
ben <benoit.pinkasfeld@gmail.com>
2014-08-04 21:31:08 +1000

    add dirty flag to bglObject

--- app/src/main/java/com/example/bEngine/object/BglObject.java
+++ app/src/main/java/com/example/bEngine/object/BglObject.java
@@ -3,14 +3,20 @@ package com.example.bEngine.object;
 import android.content.Context;
 import android.graphics.PointF;
 import android.opengl.GLES20;
+import android.opengl.Matrix;
 
+import com.example.bEngine.Brenderer;
 import com.example.bEngine.TextureCoordCalculator;
+import com.example.bEngine.scene.SceneManager;
+import com.example.bEngine.service.CollisionService;
 import com.example.bEngine.service.GlService;
 import com.example.bEngine.shader.Shader;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.util.List;
+
 import static android.opengl.GLES20.*;
 /**
  * Created by Ben on 10/10/13.
@@ -18,16 +24,71 @@ import static android.opengl.GLES20.*;
 public abstract class BglObject extends Bobject {
 
     public GlService glService;
-
-    protected boolean visible = true;
-    protected boolean disregardCam = false;
-    protected boolean collide = true;
+    public CollisionService collisionService = new CollisionService();
     protected int[] res;
+
+    public boolean visible = true;
+    protected boolean disregardCam = false;
+    public boolean collide = true;
+    protected List areaOfInterest;
+
+    protected float[] mvp = new float[16];
+    public boolean dirty;
 
     public BglObject( float x, float y, float w, float h ){
         super(x,y,w,h);
+        dirty = true;
     }
 
+    @Override
+    public void setPos(PointF pos) {
+        super.setPos(pos);
+        dirty = true;
+    }
+    @Override
+    public void setPos(float x, float y) {
+        super.setPos(x, y);
+        dirty = true;
+    }
+    @Override
+    public void setPos(Bobject o) {
+        super.setPos(o);
+        dirty = true;
+    }
+    @Override
+    public void setPos(float x, float y, float oriX, float oriY) {
+        super.setPos(x, y, oriX, oriY);
+        dirty = true;
+    }
+    @Override
+    public void setZ(float z) {
+        super.setZ(z);
+        dirty = true;
+    }
+    @Override
+    public void setSize(float w, float h) {
+        super.setSize(w, h);
+        dirty = true;
+    }
+    @Override
+    public void setAngleX(float anglex) {
+        super.setAngleX(anglex);
+        dirty = true;
+    }
+    @Override
+    public void setAngleY(float angley) {
+        super.setAngleY(angley);
+        dirty = true;
+    }
+    @Override
+    public void setAngleZ(float anglez) {
+        super.setAngleZ(anglez);
+        dirty = true;
+    }
+
+    public float[] getMvp() {
+        return mvp;
+    }
 
     public int[] getRes() {
         return res;
@@ -57,23 +118,22 @@ public abstract class BglObject extends Bobject {
         disregardCam = cam;
     }
 
-    //TODO is it bad design to have to set mannually if an object is concerne par les collisions
     public boolean isCollide() {
         return collide;
     }
-
     public void setCollide(boolean collide) {
         this.collide = collide;
     }
 
-    //TODO get rid of that!!!! shouldnt be here!!!!
-
-
-    public int getTextureState(){
-        return 0;
+    public List getAreaOfInterest() {
+        return areaOfInterest;
+    }
+    public void setAreaOfInterest(List areaOfInterest) {
+        this.areaOfInterest = areaOfInterest;
     }
 
     //TODO is it right or wrong?
     public abstract float[] getColor();
-    public abstract void collision();
+    public abstract void collision(Bobject collider, CollisionService.collisionSide colSide);
+
 }
