03b5aedb0a02d43986e044374a8564de320da63b
ben <benoit.pinkasfeld@gmail.com>
2014-10-12 13:48:17 +1100

    Fix bugs in object pool

--- app/src/main/java/com/benpinkas/bEngine/ObjectPool.java
+++ app/src/main/java/com/benpinkas/bEngine/ObjectPool.java
@@ -15,16 +15,16 @@ public class ObjectPool {
     public ObjectPool( Bobject[] obj){
         this.obj = obj;
         available = new boolean[obj.length];
-        for (boolean b : this.available){
-            b = true;
+        for (int i=0; i<available.length; i++){
+            available[i] = true;
         }
     }
 
     public void setPool( Bobject[] obj){
         this.obj = obj;
         available = new boolean[obj.length];
-        for (boolean b : this.available){
-            b = true;
+        for (int i=0; i<available.length; i++){
+            available[i] = true;
         }
     }
 
@@ -33,9 +33,12 @@ public class ObjectPool {
         for(int i=0; i<obj.length; i++) {
             if( available[i] ){
                 available[i] = false;
+                System.out.println("Object " + i + " " + " was allocated");
                 return obj[i];
             }
         }
+        System.out.println( " FAILED TO allocate !");
+
         return obj[0];
     }
 
@@ -43,11 +46,14 @@ public class ObjectPool {
 
     public boolean release(Bobject o){
         for(int i=0; i<obj.length; i++){
-            if( obj[i].equals(o) ){
-                available[i] = false;
+            if( obj[i] == o ){
+                available[i] = true;
+                System.out.println("Object " + i + " " + " was RELEASED!");
                 return true;
             }
         }
+        System.out.println( " FAILED TO RELEASE !");
+
         return false;
     }
 
