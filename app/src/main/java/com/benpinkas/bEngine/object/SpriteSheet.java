e27ef9c88f7886194ed8ed825221a7de03921a41
ben <benoit.pinkasfeld@gmail.com>
2014-11-25 21:47:47 +1100

    SpriteSheet - now accepts the duration for each Sprite

--- app/src/main/java/com/benpinkas/bEngine/object/SpriteSheet.java
+++ app/src/main/java/com/benpinkas/bEngine/object/SpriteSheet.java
@@ -9,15 +9,35 @@ public class SpriteSheet {
     private int number_of_frame_x;
     private int number_of_frame_y;
     private int number_of_frame_real;
+    private int[] frame_duration; // In frame
+    private int total_duration; // In frame
 
-    public SpriteSheet(int texture_id, int number_of_frame_x, int number_of_frame_y, int number_of_frame_real){
+    public SpriteSheet(int texture_id, int number_of_frame_x, int number_of_frame_y,
+                       int number_of_frame_real) {
         this.texture_id = texture_id;
         this.number_of_frame_x = number_of_frame_x;
         this.number_of_frame_y = number_of_frame_y;
         this.number_of_frame_real = number_of_frame_real;
+        for (int i=0; i<number_of_frame_real; i++)
+            this.frame_duration[i] = 1;
+        total_duration = number_of_frame_real;
     }
 
+    public SpriteSheet(int texture_id, int number_of_frame_x, int number_of_frame_y,
+                       int number_of_frame_real, int[] frame_duration ) {
+        this.texture_id = texture_id;
+        this.number_of_frame_x = number_of_frame_x;
+        this.number_of_frame_y = number_of_frame_y;
+        this.number_of_frame_real = number_of_frame_real;
+        this.frame_duration = frame_duration;
+        for (int i=0; i<number_of_frame_real; i++)
+            total_duration =  total_duration + frame_duration[i];
 
+    }
+
+    public int getTotal_duration(){
+        return total_duration;
+    }
 
     public int getTexture_id() {
         return texture_id;
@@ -33,5 +53,9 @@ public class SpriteSheet {
 
     public int getNumber_of_frame_real() {
         return number_of_frame_real;
+    }
+
+    public int get_current_duration(int i) {
+        return frame_duration[i];
     }
 }
