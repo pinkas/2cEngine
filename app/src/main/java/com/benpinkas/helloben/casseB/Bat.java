aff3d876afeddd17bfe84bad9d20c4c7b9a01756
ben <benoit.pinkasfeld@gmail.com>
2014-11-25 22:09:06 +1100

    Bat - the bat now goes to where the finger points with an animation

--- app/src/main/java/com/benpinkas/helloben/casseB/Bat.java
+++ app/src/main/java/com/benpinkas/helloben/casseB/Bat.java
@@ -9,19 +9,21 @@ import com.benpinkas.bEngine.object.Brectangle;
 import com.benpinkas.bEngine.service.Bcall;
 import com.benpinkas.bEngine.service.CollisionService;
 import com.benpinkas.bEngine.service.MessageManager;
+import com.benpinkas.helloben.casseB.wizard.Projectile;
 
 /**
  * Created by Ben on 14-Jul-14.
  */
 public class Bat extends BglSprite {
 
-    private boolean touched;
     private final static float NORMAL_SIZE_W = 0.25f;
-    private final static float NORMAL_SIZE_H = 0.03f;
+    private final static float NORMAL_SIZE_H = 0.04f;
     private final static float ENLARGE_FACTOR = 2f;
+    private float toGoX = 0;
+    private float toGoY;
 
     public Bat(){
-        super(0.4f, 0.9f, NORMAL_SIZE_W, NORMAL_SIZE_H, new int[] {R.drawable.bat});
+        super(0.4f, 0.85f, NORMAL_SIZE_W, NORMAL_SIZE_H, new int[] {R.drawable.bat});
 
         MessageManager.addListener("bat_down", new Bcall<Void>() {
             public Void call(Object o) {
@@ -48,12 +50,25 @@ public class Bat extends BglSprite {
 
     }
 
-    public void moveBatToFinger(){
-        setPos(InputStatus.touchXscr/(float) Brenderer.getScreenW() - 0.5f*getSizeW(), getPosY());
+    public void setToGoPos() {
+        this.toGoX = InputStatus.touchXscr / (float) Brenderer.getScreenW() - 0.5f * getSizeW();
+        this.toGoY = getPosY();
     }
 
     public void update(float dt){
+        if (toGoX != 0 ) {
+            float x = getPosX();
+            float y = getPosY();
+            float dx = (toGoX - x) / 3;
+            setPos(x + dx, y);
+        }
     }
 
+    @Override
+    public void collision(Bobject collider, CollisionService.collisionSide cs) {
+        if ( collider instanceof Projectile) {
+            MessageManager.sendMessage("bat_hit_by_projectile");
+        }
+    }
 
 }
