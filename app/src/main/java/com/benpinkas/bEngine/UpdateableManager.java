f3ba5c946e52e1edd065bf7c9af8c1f80a5f4b6d
ben <benoit.pinkasfeld@gmail.com>
2015-10-08 22:45:42 +1100

    add updatableManager

--- app/src/main/java/com/benpinkas/bEngine/UpdateableManager.java
+++ app/src/main/java/com/benpinkas/bEngine/UpdateableManager.java
@@ -1,7 +1,33 @@
 package com.benpinkas.bEngine;
 
-/**
- * Created by ben on 6/3/2015.
- */
+import com.benpinkas.bEngine.object.Updatable;
+
+import java.util.ArrayList;
+
 public class UpdateableManager {
+
+    private static ArrayList<Updatable> toUpdate = new ArrayList<Updatable>();
+    private static ArrayList<Updatable> stopToUpdate = new ArrayList<Updatable>();
+
+
+    public static void addToUpdate( Updatable u ){
+        toUpdate.add( u );
+    }
+
+    public static void update(float dt){
+        for ( Updatable u : toUpdate ) {
+            if( !u.update(dt) ) {
+                stopToUpdate.add(u);
+            }
+        }
+
+        for ( Updatable u : stopToUpdate ) {
+            // endcallback calls the desInit of the action, since an action may call
+            // another (which add it to 'toUpdate' we call the endCallback here so that it does
+            // add something to toUpdate while going throught it
+            u.endCallback();
+            toUpdate.remove(u);
+        }
+        stopToUpdate.clear();
+    }
 }
